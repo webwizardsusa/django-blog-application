d15211bac8390a02a1b8aee975f15013ce97d0d3
webwizardsusa <webwizardsusa@gmail.com>
2025-01-24 01:46:09 +0530

    dynamic content updated - 149

--- public_site/urls.py
+++ public_site/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('blog/<int:id>/', views.blog_detail, name='blog_detail'),
+    path('category/<str:name>/', views.blog_category, name='blog_category'),
     # path("blogs/", views.blog_list, name='blog_list'),
     # path('blogs/<slug:slug>/', views.blog_detail, name='blog_detail'),
 ]
--- public_site/views.py
+++ public_site/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from web_admin.blog.models import Blog
+from web_admin.blog.models import Category
 
 def home(request):
-    blogs = Blog.objects.filter(is_published=True).order_by("-created_at")[:5]
-    return render(request, "public_site/home.html", {"blogs": blogs})
+    # blogs = Blog.objects.filter(is_published=True).order_by("-created_at")[:5]
+    featured_blog = Blog.objects.filter(is_published=True).order_by("-created_at").first()
+    recent_blogs = Blog.objects.filter(is_published=True).order_by("-created_at")[1:6]
+    return render(request, "public_site/home.html", {"featured_blog": featured_blog, "recent_blogs": recent_blogs})
+
+def blog_detail(request, id):
+    blog = get_object_or_404(Blog, id=id)
+    return render(request, 'public_site/blog_detail.html', {'blog': blog})
+
+def blog_category(request, name):
+    category = get_object_or_404(Category, name=name)
+    blogs = category.blogs.all()
+    return render(request, 'public_site/blog_category.html', {'category': category, 'blogs': blogs})
