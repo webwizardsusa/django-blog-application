bba64136a20a5e21e843326826c990394e6ed4a4
webwizardsusa <webwizardsusa@gmail.com>
2025-02-18 19:39:21 +0530

    Update without settings - 160

--- public_site/contact/forms.py
+++ public_site/contact/forms.py
@@ -16,34 +16,34 @@ class ContactForm(forms.ModelForm):
             "message": forms.Textarea(attrs={"class": "form-control", "rows": 5, "placeholder": "Enter Message"}),
         }
         
-    # def clean_phone(self):
-    #     phone = self.cleaned_data.get('phone')
-    #     if not phone.isdigit():
-    #         raise forms.ValidationError("Phone number should contain only digits.")
-    #     if len(phone) < 10 or len(phone) > 15:
-    #         raise forms.ValidationError("Phone number must be between 10 and 15 digits.")
-    #     return phone
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+        if not phone.isdigit():
+            raise forms.ValidationError("Phone number should contain only digits.")
+        if len(phone) < 10 or len(phone) > 15:
+            raise forms.ValidationError("Phone number must be between 10 and 15 digits.")
+        return phone
     
-    # def clean_first_name(self):
-    #     first_name = self.cleaned_data.get('first_name')
-    #     if re.search(r'\d', first_name):
-    #         raise forms.ValidationError("First Name should contain only Alphabetic.")
-    #     return first_name
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        if re.search(r'\d', first_name):
+            raise forms.ValidationError("First Name should contain only Alphabetic.")
+        return first_name
     
-    # def clean_last_name(self):
-    #     last_name = self.cleaned_data.get('last_name')
-    #     if re.search(r'\d', last_name):
-    #         raise forms.ValidationError("Last Name should contain only Alphabetic.")
-    #     return last_name
+    def clean_last_name(self):
+        last_name = self.cleaned_data.get('last_name')
+        if re.search(r'\d', last_name):
+            raise forms.ValidationError("Last Name should contain only Alphabetic.")
+        return last_name
     
-    # def clean_message(self):
-    #     message = self.cleaned_data.get('message')
-    #     if len(message) < 100:
-    #         raise forms.ValidationError("Please write message minimum 100 characters")
-    #     return message
+    def clean_message(self):
+        message = self.cleaned_data.get('message')
+        if len(message) < 100:
+            raise forms.ValidationError("Please write message minimum 100 characters")
+        return message
     
-    # def clean_email(self):
-    #     email = self.cleaned_data.get("email")
-    #     if self.instance.pk:
-    #         qs = qs.exclude(pk=self.instance.pk)
-    #     return email
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if self.instance.pk:
+            qs = qs.exclude(pk=self.instance.pk)
+        return email
