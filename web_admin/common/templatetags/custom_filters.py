fea1c542e5702637c83c519ec575214369a5e261
webwizardsusa <webwizardsusa@gmail.com>
2025-03-11 17:16:26 +0530

    Date format changes-148

--- web_admin/common/templatetags/custom_filters.py
+++ web_admin/common/templatetags/custom_filters.py
@@ -4,7 +4,7 @@ from datetime import datetime, date
 register = template.Library()
 
 @register.filter
-def date_format(value, fmt="%d/%m/%Y"):
+def date_format(value, fmt="%m/%d/%Y"):
     if isinstance(value, (datetime, date)):
         return value.strftime(fmt)
     return value
